8164ed55b8d286bda9522aa6015a3ae0d2bc6fdc
Hadi <hadi_fr94@hotmail.com>
2024-10-11 16:54:38 -0400

    modified:   index.html 	modified:   pages/index.js 	deleted:    validity.js

--- pages/index.js
+++ pages/index.js
@@ -189,3 +189,82 @@ document.addEventListener("DOMContentLoaded", () => {
   renderInitialCards();
   enableValidation(config);
 });
+
+// validity.js
+
+function showInputError(formElement, inputElement, errorMessage, options) {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.add(options.inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(options.errorClass);
+}
+
+function hideInputError(formElement, inputElement, options) {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove(options.inputErrorClass);
+  errorElement.classList.remove(options.errorClass);
+  errorElement.textContent = "";
+}
+
+function checkInputValidity(formElement, inputElement, options) {
+  if (!inputElement.validity.valid) {
+    showInputError(
+      formElement,
+      inputElement,
+      inputElement.validationMessage,
+      options
+    );
+  } else {
+    hideInputError(formElement, inputElement, options);
+  }
+}
+
+function hasInvalidInput(inputList) {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+}
+
+function toggleButtonState(inputList, buttonElement, options) {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add(options.inactiveButtonClass);
+    buttonElement.disabled = true;
+  } else {
+    buttonElement.classList.remove(options.inactiveButtonClass);
+    buttonElement.disabled = false;
+  }
+}
+
+function setEventListeners(formElement, options) {
+  const inputList = Array.from(
+    formElement.querySelectorAll(options.inputSelector)
+  );
+  const buttonElement = formElement.querySelector(options.submitButtonSelector);
+  toggleButtonState(inputList, buttonElement, options);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", () => {
+      checkInputValidity(formElement, inputElement, options);
+      toggleButtonState(inputList, buttonElement, options);
+    });
+  });
+}
+
+function enableValidation(options) {
+  const formList = Array.from(document.querySelectorAll(options.formSelector));
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement, options);
+  });
+}
+
+export {
+  showInputError,
+  hideInputError,
+  checkInputValidity,
+  hasInvalidInput,
+  toggleButtonState,
+  setEventListeners,
+  enableValidation,
+};
