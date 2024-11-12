09a2e08f46c79b5db37e70bcf3c205c4012a15cb
Hadi <hadi_fr94@hotmail.com>
2024-11-12 08:25:58 -0500

    modified:   bebel.config.js 	deleted:    blocks/card.css 	deleted:    blocks/cards.css 	deleted:    blocks/footer.css 	deleted:    blocks/header.css 	deleted:    blocks/modal.css 	deleted:    blocks/page.css 	deleted:    blocks/profile.css 	deleted:    components/Card.js 	deleted:    components/FormValidator.js 	deleted:    images/Close Icon.svg 	deleted:    images/Group.svg 	deleted:    images/Trash.svg 	deleted:    images/Vector-1.svg 	deleted:    images/Vector.png 	deleted:    images/addbutton.svg 	deleted:    images/background_header.svg 	deleted:    images/background_menu.svg 	deleted:    images/background_recipes.svg 	deleted:    images/background_reservation.svg 	deleted:    images/bald-mountains.png 	deleted:    images/check.svg 	deleted:    images/editbutton.svg 	deleted:    images/facebook_white.svg 	deleted:    images/favicon_circles.ico 	deleted:    images/favicon_coffee_bean.ico 	deleted:    images/headerlogo.svg 	deleted:    images/inside_coffee_shop.png 	deleted:    images/instagram_white.svg 	deleted:    images/jacques-cousteau.jpg 	deleted:    images/lago-di-braies.png 	deleted:    images/lake-louise.png 	deleted:    images/latemar.png 	deleted:    images/like-button-active.svg 	deleted:    images/logo_coffeeshop_dark.svg 	deleted:    images/logo_coffeeshop_light.svg 	deleted:    images/vanoise-national-park.png 	deleted:    images/yosemite-valley.jpg 	deleted:    index.html 	modified:   package-lock.json 	modified:   package.json 	deleted:    pages/index.css 	deleted:    pages/index.js 	deleted:    scripts/Popup.js 	deleted:    scripts/PopupWithForm.js 	deleted:    vendor/fonts.css 	deleted:    vendor/fonts/Inter-Black.woff2 	deleted:    vendor/fonts/Inter-Medium.woff2 	deleted:    vendor/fonts/Inter-Regular.woff2 	deleted:    vendor/normalize.css

--- bebel.config.js
+++ bebel.config.js
@@ -1,13 +1,56 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: {
+    main: './src/pages/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '',
+  },
+  target: ['web', 'es5'],
+  stats: 'errors-only',
+  mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+};
+
 const presets = [
   [
     "@babel/preset-env",
     {
-      // preset you want to use
-      // browser versions in which we want our code supported
       targets: "defaults, not IE 11, not dead",
-
-      // use polyfills for the browsers specified in the above targets option
-      // Babel uses polyfills from the core-js library
       useBuiltIns: "entry",
       corejs: "^3",
     },
@@ -15,3 +58,29 @@ const presets = [
 ];
 
 module.exports = { presets };
+
+{
+  "name": "your-project-name",
+  "version": "1.0.0",
+  "description": "Your project description",
+  "main": "index.js",
+  "scripts": {
+    "start": "webpack serve --mode development",
+    "build": "webpack --mode production"
+  },
+  "devDependencies": {
+    "@babel/core": "^7.14.6",
+    "@babel/preset-env": "^7.14.7",
+    "babel-loader": "^8.2.2",
+    "clean-webpack-plugin": "^3.0.0",
+    "core-js": "^3.15.1",
+    "css-loader": "^5.2.6",
+    "html-webpack-plugin": "^5.3.2",
+    "mini-css-extract-plugin": "^2.1.0",
+    "style-loader": "^2.0.0",
+    "webpack": "^5.38.1",
+    "webpack-cli": "^4.7.2",
+    "webpack-dev-server": "^3.11.2"
+  },
+  "dependencies": {}
+}
