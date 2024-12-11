7b05988304ba9cf1a00f1bbb9f9c8b999f4edba9
Hadi <hadi_fr94@hotmail.com>
2024-12-10 23:59:57 -0500

    modified:   src/components/PopupWithConfirmation.js

--- src/components/PopupWithConfirmation.js
+++ src/components/PopupWithConfirmation.js
@@ -1,7 +1,17 @@
 import Popup from "./Popup.js";
- 
-this._submitButton = this._popupform.querySelector(".modal__submit-button");
 
+export default class PopupWithConfirmation extends Popup {
+  constructor({ popupSelector }) {
+    const popupElement = document.querySelector(popupSelector);
+    if (!popupElement) {
+      throw new Error(`Element with selector "${popupSelector}" not found.`);
+    }
+    super({ popupSelector: popupSelector });
+    this._popupform = popupElement.querySelector(".modal__form");
+    this._submitButton = this._popupform.querySelector(".modal__submit-button");
+    this._submitButtonText = this._submitButton.textContent;
+    this._handleForSubmit = null;
+  }
 
   setSubmitAction(action) {
     this._handleForSubmit = action;
@@ -25,5 +35,5 @@ this._submitButton = this._popupform.querySelector(".modal__submit-button");
       }
     });
     super.setEventListeners();
-  };
+  }
 }
